8c8da2539aeb9afd4d5ab4a20b3690bb60f3649b
Pietro Barone <pietro.barone@edu.unito.it>
2020-07-11 13:24:09 +0200

    intersect observer

--- fun.js
+++ fun.js
@@ -1,24 +1,55 @@
-const navbar = document.querySelector(".navbar");
-const home = document.querySelector("#home");
-
-const homeOptions = {
-  rootMargin: "-2px 0px 0px 0px"
-};
-
-const homeObserver = new IntersectionObserver(function(
-  entries,
-  homeObserver
-) {
+const home = document.querySelector("#home-text");
+const homeObserver = new IntersectionObserver(function(entries) {
   entries.forEach(entry => {
-    if (!entry.isIntersecting){
-        $(".highlight").addClass("afterHigh");
+    console.log(entry);
+    if (entry.isIntersecting){
+        $("#h").css("color", "rgb(204, 109, 109)");
     }else{
-        $(".highlight").removeClass("afterHigh");
+        $("#h").css("color", "white");
     }
   });
-},
-homeOptions);
-
+});
 homeObserver.observe(home);
 
 
+const about = document.querySelector("#am");
+const aboutObserver = new IntersectionObserver(function(entries) {
+  entries.forEach(entry => {
+    console.log(entry);
+    if (entry.isIntersecting){
+        $(".highlight").addClass("afterHigh");
+        $("#a").css("color", "rgb(204, 109, 109)");
+    }else{
+        $(".highlight").removeClass("afterHigh");
+        $("#a").css("color", "white");
+    }
+  });
+});
+aboutObserver.observe(about);
+
+
+const skills = document.querySelector("#sk");
+const skillsObserver = new IntersectionObserver(function(entries) {
+  entries.forEach(entry => {
+    console.log(entry);
+    if (entry.isIntersecting){
+        $("#s").css("color", "rgb(204, 109, 109)");
+    }else{
+        $("#s").css("color", "white");
+    }
+  });
+});
+skillsObserver.observe(skills);
+
+const contact = document.querySelector("#contact-title");
+const contactObserver = new IntersectionObserver(function(entries) {
+  entries.forEach(entry => {
+    console.log(entry);
+    if (entry.isIntersecting){
+        $("#c").css("color", "rgb(204, 109, 109)");
+    }else{
+        $("#c").css("color", "white");
+    }
+  });
+});
+contactObserver.observe(contact);
